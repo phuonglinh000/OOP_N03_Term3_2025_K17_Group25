4808036205b65480938014b08ce2944b063b8bfa
phuonglinh000 <23010562@st.phenikaa-uni.edu.vn>
2025-05-09 01:46:14 +0000

    update test

---
+++ test /userTest.java
@@ -0,0 +1,14 @@
+public class userTest {
+    string id ;
+    string name ; 
+    string email ;
+    
+    public User (string id , string name , string email ){
+        this.id = id ;
+        this.name = name ;
+        this email = email ;
+    }
+    public string {
+        
+    }
+}
