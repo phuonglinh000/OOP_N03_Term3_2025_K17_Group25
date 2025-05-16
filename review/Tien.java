3b11983bad9ca77b8cb5f294983200f3bcdef598
QuynhAnh-31 <quynhanhanh31122005@gmail.com>
2025-05-16 00:54:14 +0000

    xong ex21,22

---
+++ review/Tien.java
@@ -0,0 +1,12 @@
+package review;
+
+public enum Tien{
+    mot_nghin,
+    hai_nghin,
+    nam_nghin,
+    muoi_nghin,
+    hai_muoi_nghin,
+    nam_muoi_nghin
+}
+
+
---
+++ review/ex21.java
@@ -0,0 +1,8 @@
+package review;
+
+public class ex21 {
+    public static void main(String[] args) {
+        for (Tien t : Tien.values())
+            System.out.println(t + ", ordinal " + t.ordinal());
+    }
+}
