eb8d5c77cd9136752670988a9887664523241522
QuynhAnh-31 <quynhanhanh31122005@gmail.com>
2025-05-30 00:54:19 +0000

    user

--- src/User.java
+++ src/User.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class User {
     String id;
     String name; 
@@ -14,4 +17,22 @@ public class User {
         System.out.println("Tên người dùng: " + name);
         System.out.println("Email: " + email);
     }
+    public void searchBookByTitle(List<Book> books, Scanner scanner) {
+        System.out.println("Nhập từ khóa tìm tên sách:");
+        String keyword = scanner.nextLine().toLowerCase();
+        boolean found = false;
+
+        System.out.println("\n Kết quả tìm kiếm:");
+        for (Book book : books) {
+            if (book.title.toLowerCase().contains(keyword)) {
+                book.displayBook();
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Không tìm thấy sách nào.");
+        }
+    }
 }
+
