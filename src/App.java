f6b187112fa7e5dd9fcac1c3dcbeebf5aff222d2
QuynhAnh-31 <quynhanhanh31122005@gmail.com>
2025-05-30 02:03:18 +0000

    qa

--- src/App.java
+++ src/App.java
@@ -1,14 +1,5 @@
-import review.ex21_QA;
-
 public class App {
-    public static void main(String[] args){
-        System.out.println("test main");
-
-        ex21_QA.test();
-        TestEx3_QA.test();
-        BookListTest.test();
+    public static void main(String[] args) {
         UserTest.test();
-
-
     }
 }
